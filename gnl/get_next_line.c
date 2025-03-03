5a9388b9ff4502ea4c157558d47b9e434d51c78b
Pablo Romero Fernandez <pablrome@c1r7s3.42madrid.com>
2025-03-03 18:53:12 +0100

    Null Check error

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pablrome <pablrome@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 17:09:20 by pablrome          #+#    #+#             */
-/*   Updated: 2025/03/03 14:29:38 by pablrome         ###   ########.fr       */
+/*   Updated: 2025/03/03 18:52:45 by pablrome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,17 @@ static int	read_and_append(int fd, char **buffer)
 
 	temp_buffer = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
 	if (!temp_buffer)
-		return (0);
+		return (-1);
 	bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
 	if (bytes_read > 0)
 	{
 		temp_buffer[bytes_read] = '\0';
 		new_buf = ft_strjoin(*buffer, temp_buffer);
+		if (!new_buf)
+		{
+			free(temp_buffer);
+			return (-1);
+		}
 		free(*buffer);
 		*buffer = new_buf;
 	}
@@ -36,27 +41,31 @@ static int	read_and_append(int fd, char **buffer)
 
 static char	*extract_line(char **buffer)
 {
-    char	*aux;
-    char	*line;
-    int		i;
+	char	*aux;
+	char	*line;
+	int		i;
+	char	*temp;
 
 	i = 0;
-    while ((*buffer)[i] != '\0' && (*buffer)[i] != '\n')
-        i++;
-    line = ft_substr(*buffer, 0, i);
-    aux = ft_strchr(*buffer, '\n');
-    if (aux)
-    {
-        char *temp = ft_strdup(aux + 1);
-        free(*buffer);
-        *buffer = temp;
-    }
-    else
-    {
-        free(*buffer);
-        *buffer = NULL;
-    }
-    return (line);
+	while ((*buffer)[i] != '\0' && (*buffer)[i] != '\n')
+		i++;
+	if ((*buffer)[i] == '\n')
+		line = ft_substr(*buffer, 0, i + 1);
+	else
+		line = ft_substr(*buffer, 0, i);
+	aux = ft_strchr(*buffer, '\n');
+	if (aux)
+	{
+		temp = ft_strdup(aux + 1);
+		free(*buffer);
+		*buffer = temp;
+	}
+	else
+	{
+		free(*buffer);
+		*buffer = NULL;
+	}
+	return (line);
 }
 
 char	*get_next_line(int fd)
@@ -66,13 +75,13 @@ char	*get_next_line(int fd)
 	int			bytes_read;
 
 	bytes_read = 1;
-	if (BUFFER_SIZE  <= 0 || fd < 0)
+	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
 	if (!buffer)
 		buffer = ft_calloc(sizeof(char), 1);
 	if (!buffer)
 		return (NULL);
-	while (bytes_read != 0 && !ft_strchr(buffer, '\n'))
+	while (bytes_read != 0 && buffer && !ft_strchr(buffer, '\n'))
 	{
 		bytes_read = read_and_append(fd, &buffer);
 		if (bytes_read <= 0)
@@ -89,8 +98,8 @@ char	*get_next_line(int fd)
 
 // int main(void)
 // {
-//     int fd;
-//     char *line;
+//     int		fd;
+//     char	*line;
 
 //     fd = open("archivo.txt", O_RDONLY);
 //     if (fd < 0)
@@ -98,13 +107,11 @@ char	*get_next_line(int fd)
 //         perror("Error abriendo archivo.txt");
 //         return (1);
 //     }
-    
 //     while ((line = get_next_line(fd)) != NULL)
 //     {
-//         printf("%s\n", line);
+//         printf("%s", line);
 //         free(line);
 //     }
-    
 //     close(fd);
 //     return (0);
 // }
